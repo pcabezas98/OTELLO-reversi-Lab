9f1b4984f92c46ad0c7f1f033a9c800db201338b
Sebastian <s.espinozamartnez1@uandresbello.edu>
2020-10-30 18:54:16 -0300

    avance js

--- static/juego.js
+++ static/juego.js
@@ -1,10 +1,16 @@
-console.log('hola mundo');
+var x = 3;
+var posx = x.toString();
+var y = 4;
+var posy = y.toString();    
+var concat = posx + posy; 
 
+console.log(concat);
 
-function cambiar_ficha(){
-    $('#cell_3_2').removeClass('cell black attackable').addClass('cell black');
-    
+function cambiar_ficha(concat){
+    console.log(concat);
+    console.log("paso");
+    $(concat).removeClass('cell empty').addClass('cell white'); 
 }
 
-
+$('#00').removeClass('cell empty').addClass('cell black');
 
